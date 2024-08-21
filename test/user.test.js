3cca5239a34c9d2ba684169e7a034e334ac5f51a
Ashraf, Anas <Anas.Ashraf@easy.de>
2024-08-21 19:40:45 +0200

    added some unit tests

---
+++ test/user.test.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+
+
+describe("for_testing",()=>{
+    describe('add', ()=>{
+        it("this is only a test test", ()=>{
+            expect(1+1).to.equal(2)
+        })
+    })
+} )
